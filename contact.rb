5b665ecdcff57f1580465ce24f540ba2a6765f3a
Daniel Feller <70229820+danFeller@users.noreply.github.com>
2024-06-27 22:15:46 +0000

    instance variable removed due to redundancy

--- contact.rb
+++ contact.rb
@@ -1,10 +1,8 @@
 class Contact
-  @@num_of_contacts = 0
   def initialize(number, name, contact_type)
     @number = number
     @name = name
     @contact_type = contact_type
-    @@num_of_contacts = @@num_of_contacts + 1
   end
   
   def format
